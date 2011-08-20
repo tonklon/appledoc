3792468b2bff59f94aae58a7ab9ee58a465e2438
Tobias Klonk <tobias.klonk@tklonk.net>
2011-08-20 15:53:52 +0200

    create an index for the parser create a translationalUnit and a file for each run

--- Parsing/GBClangBasedParser.m
+++ Parsing/GBClangBasedParser.m
@@ -10,11 +10,23 @@
 #import "GBStore.h"
 #import "GBApplicationSettingsProvider.h"
 #import "GBDataObjects.h"
+#import "clang-c/Index.h"
 
 @interface GBClangBasedParser ()
 
 @property (retain) GBStore *store;
 @property (retain) GBApplicationSettingsProvider *settings;
+@property (assign) CXIndex index;
+@property (assign) CXTranslationUnit translationUnit;
+@property (assign) CXFile file;
+
+@end
+
+@interface GBClangBasedParser (ClangInterface)
+
+- (NSArray*)clangArgumentsForFilename:(NSString*)filename;
+- (void)createTranslationUnitWithContents:(NSString*)input filename:(NSString*)filename
+                                arguments:(NSArray*)arguments;
 
 @end
 
@@ -34,6 +46,7 @@
 	self = [super init];
 	if (self) {
 		self.settings = settingsProvider;
+    self.index = clang_createIndex(1, 0);
 	}
 	return self;
 }
@@ -48,12 +61,75 @@
 	GBLogDebug(@"Parsing objective-c objects...");
 	self.store = store;
 
-  // Do it.
+  NSArray* arguments = [self clangArgumentsForFilename:filename];
+  [self createTranslationUnitWithContents:input filename:filename arguments:arguments];
 }
 
 #pragma mark Properties
 
 @synthesize settings;
 @synthesize store;
+@synthesize index;
+@synthesize translationUnit;
+@synthesize file;
+
+- (void)dealloc {
+  clang_disposeIndex(self.index);
+  clang_disposeTranslationUnit(self.translationUnit);
+  
+  [super dealloc];
+}
+
+@end
+
+@implementation GBClangBasedParser (ClangInterface)
+
+- (NSArray*)clangArgumentsForFilename:(NSString*)filename {
+  return [NSArray arrayWithObjects:
+          @"-isysroot/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.0.sdk" ,
+          @"-xobjective-c",
+          @"-std=gnu99",
+          @"-D__IPHONE_OS_VERSION_MIN_REQUIRED=30000",
+          nil];
+}
+
+- (void)createTranslationUnitWithContents:(NSString*)input
+                                 filename:(NSString*)filename
+                                arguments:(NSArray*)arguments {
+  
+  const char *fn = [filename UTF8String];
+	struct CXUnsavedFile unsaved[] = {
+		{fn, [input UTF8String], [input length]},
+		{NULL, NULL, 0}};
+  
+	self.file = NULL;
+	if (NULL != self.translationUnit)
+	{
+    clang_disposeTranslationUnit(self.translationUnit);
+  }
+  
+  const char *mainFile = fn;
+  unsigned argc = [arguments count];
+  const char *argv[argc];
+  int i=0;
+  for (NSString *arg in arguments)
+  {
+    argv[i++] = [arg UTF8String];
+  }
+  int unsavedCount = 1;
+  if ([@"h" isEqualToString: [filename pathExtension]])
+  {
+    unsaved[1].Filename = "/tmp/foo.m";
+    unsaved[1].Contents = [[NSString stringWithFormat: @"#import \"%@\"\n", filename] UTF8String];
+    unsaved[1].Length = strlen(unsaved[1].Contents);
+    mainFile = unsaved[1].Filename;
+    unsavedCount = 2;
+  }
+  self.translationUnit =
+  clang_createTranslationUnitFromSourceFile(self.index, fn, argc, argv, 0, unsaved);
+  
+  self.file = clang_getFile(self.translationUnit, fn);
+  
+}
 
 @end
